1d9ac13d7683d535804bad9dd7db0eeaff1b4997
ezkript <107833602+ezkript@users.noreply.github.com>
2026-08-17 23:35:39 -0300

    add dynamic sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,11 @@
 import { MetadataRoute } from 'next';
+import { projectsData } from '@/lib/projects-data';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl =
     process.env.NEXT_PUBLIC_SITE_URL || 'https://jlc-fire.vercel.app';
 
-  const routes = [
+  const staticRoutes = [
     '',
     '/about',
     '/certifications',
@@ -21,10 +22,21 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/services/software-graphic'
   ];
 
-  return routes.map(route => ({
+  const staticSitemap: MetadataRoute.Sitemap = staticRoutes.map(route => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString(),
     changeFrequency: route === '' ? 'monthly' : 'yearly',
     priority: route === '' ? 1 : 0.8
   }));
+
+  const dynamicProjectsSitemap: MetadataRoute.Sitemap = projectsData.map(
+    project => ({
+      url: `${baseUrl}/projects/${project.id}`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.64
+    })
+  );
+
+  return [...staticSitemap, ...dynamicProjectsSitemap];
 }
